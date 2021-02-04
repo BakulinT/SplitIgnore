fb14d711f330a906386673b6757e6907d4f344a2
Artem <infi@mmail.com>
2021-02-04 17:57:48 +0300

    Создание класса SplitIgnore

---
+++ SplitIgnore/SplitIgnore/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SplitIgnore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = SplitIgnore.split(@"*'** 8*** ' *", ' ', '\'');
+            foreach (string s in str)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ SplitIgnore/SplitIgnore/SplitIgnore.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SplitIgnore
+{
+    class SplitIgnore
+    {
+        public static string[] split(string str, char separator, char marker)
+        {
+            ArrayList arr = new ArrayList();
+
+            int start = 0, len = str.Length;
+
+            for (int i = 1; i <= len; i++)
+            {
+                if (str[i - 1] == marker)
+                {
+                    for (int j = i; j < len; j++)
+                    {
+                        if (str[j] == marker)
+                        {
+                            arr.Add(str.Substring(i, j - i));
+                            start = j + 2;
+                            i = j + 2;
+                            break;
+                        }
+                    }
+                }
+                if ( start != i && (i == len || str[i] == separator) )
+                {
+                    arr.Add(str.Substring(start, i - start));
+                    start = i + 1;
+                }
+            }
+            return (string[]) arr.ToArray(typeof(string));
+        }
+
+    }
+}
